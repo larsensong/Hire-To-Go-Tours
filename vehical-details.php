6d24ad678c3ce306185e5cee298db716898812e9
larsen_dev <53650985+larsensong@users.noreply.github.com>
2023-10-02 23:14:31 +0300

    Update vehical-details.php

--- vehical-details.php
+++ vehical-details.php
@@ -57,7 +57,7 @@ echo "<script>alert('Something went wrong. Please try again');</script>";
 <!--FontAwesome Font Style -->
 <link href="assets/css/font-awesome.min.css" rel="stylesheet">
 
-<!-- SWITCHER -->
+<!-- SWITCHER. -->
 		<link rel="stylesheet" id="switcher-css" type="text/css" href="assets/switcher/css/switcher.css" media="all" />
 		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/red.css" title="red" media="all" data-default-color="true" />
 		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/orange.css" title="orange" media="all" />
